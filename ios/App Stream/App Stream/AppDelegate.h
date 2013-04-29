2bd8487f17f0f033d7d62247647351eeb540f73d
Paul de Lange <paul@carnivallabs.com>
2013-04-29 22:31:52 +0200

    Main Project

---
+++ ios/App Stream/App Stream/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  App Stream
+//
+//  Created by de Lange Paul on 4/29/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
