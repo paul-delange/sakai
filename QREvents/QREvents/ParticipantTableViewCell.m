04326cc0357900db228a269a1f1a1f77e535a716
Paul de Lange <inakano.otokonoko@gmail.com>
2013-09-25 13:22:46 +0200

    importing qrcode base

---
+++ QREvents/QREvents/ParticipantTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  ParticipantTableViewCell.m
+//  QREvents
+//
+//  Created by Paul De Lange on 25/09/13.
+//  Copyright (c) 2013 Toshimoto Sakai. All rights reserved.
+//
+
+#import "ParticipantTableViewCell.h"
+
+@implementation ParticipantTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ QREvents/QREvents/ParticipantsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ParticipantsViewController.h
+//  QREvents
+//
+//  Created by Paul De Lange on 25/09/13.
+//  Copyright (c) 2013 Toshimoto Sakai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ParticipantsViewController : UITableViewController
+
+@end
