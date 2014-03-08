90ae6dfa48c015c84e39b899d42208e1194e36c3
Paul de Lange <pauldelange@pauls-mbp.home>
2014-03-09 00:48:40 +1000

    pm2.5 project

---
+++ PM2.5/pm25/pm25/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pm25
+//
+//  Created by Paul de Lange on 9/03/2014.
+//  Copyright (c) 2014 Chesterford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PM2.5/pm25/pm25Tests/pm25Tests.m
@@ -0,0 +1,34 @@
+//
+//  pm25Tests.m
+//  pm25Tests
+//
+//  Created by Paul de Lange on 9/03/2014.
+//  Copyright (c) 2014 Chesterford. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface pm25Tests : XCTestCase
+
+@end
+
+@implementation pm25Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
