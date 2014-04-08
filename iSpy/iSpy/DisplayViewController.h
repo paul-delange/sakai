757e6f7b1a7c9a0dfb8df5df150ebaa8c88c0e66
Paul de Lange <pauldelange@pauls-mbp.home>
2014-04-08 19:03:39 +1000

    iSpy project

---
+++ iSpy/iSpy/DisplayViewController.h
@@ -0,0 +1,13 @@
+//
+//  DisplayViewController.h
+//  iSpy
+//
+//  Created by Paul de Lange on 8/04/2014.
+//  Copyright (c) 2014 Chesterford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DisplayViewController : UIViewController
+
+@end
---
+++ iSpy/iSpy/iSpy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iSpy
+//
+//  Created by Paul de Lange on 8/04/2014.
+//  Copyright (c) 2014 Chesterford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
