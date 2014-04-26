d5a6ae546551d2a0fdd86a5235da8e0ef6631e09
Paul de Lange <pauldelange@pauls-mbp.home>
2014-04-26 23:12:29 +1000

    adding demo view

--- iSpy/Production/CustomerCounter/CustomerCounter/DemoViewController.m
+++ iSpy/Production/CustomerCounter/CustomerCounter/DemoViewController.m
@@ -8,42 +8,91 @@
 
 #import "DemoViewController.h"
 
-@interface DemoViewController ()
+#import "CustomerDetector.h"
+
+@import AVFoundation;
+
+@interface DemoViewController () <CustomerDetectorDelegate> {
+    CustomerDetector*   _detector;
+    AVCaptureVideoPreviewLayer* _previewLayer;
+}
 
 @end
 
 @implementation DemoViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
+#pragma mark - NSObject
+- (instancetype) initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder: aDecoder];
+    if( self ) {
+        _detector = [CustomerDetector new];
+        _detector.delegate = self;
     }
     return self;
 }
 
+#pragma mark - UIViewController
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    AVCaptureVideoPreviewLayer* previewLayer = [_detector previewLayer];
+    [self.view.layer addSublayer: previewLayer];
+    _previewLayer = previewLayer;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear: animated];
+    
+    [_detector start];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void) viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear: animated];
+    
+    [_detector stop];
 }
-*/
+
+- (void) viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    _previewLayer.frame = self.view.bounds;
+}
+
+#pragma mark - CustomerDetectorDelegate
+- (void) customerDetector:(CustomerDetector *)detector detectedCustomers:(NSSet *)customers {
+    NSLog(@"Detected: %@", customers);
+}
+
+- (void) customerDetector:(CustomerDetector *)detector encounteredError:(NSError *)error {
+    switch (error.code) {
+        case kCustomerCounterErrorCanNotAddMetadataOutput:
+        {
+            NSString* title = NSLocalizedString(@"Initialization Error", @"");
+            NSString* format = NSLocalizedString(@"%@ could not connect to the device sensors. Please restart the app and try again.", @"");
+            UIAlertView* alert = [[UIAlertView alloc] initWithTitle: title
+                                                            message: [NSString stringWithFormat: format, kAppName()]
+                                                           delegate: nil
+                                                  cancelButtonTitle: NSLocalizedString(@"OK", @"")
+                                                  otherButtonTitles: nil];
+            [alert show];
+            break;
+        }
+        case kCustomerCounterErrorNoFaceRecognition:
+        {
+            NSString* title = NSLocalizedString(@"Incompatible device", @"");
+            NSString* format = NSLocalizedString(@"%@ is not supported on this device! Please try using an iPad 2 or later device.", @"");
+            UIAlertView* alert = [[UIAlertView alloc] initWithTitle: title
+                                                            message: [NSString stringWithFormat: format, kAppName()]
+                                                           delegate: nil
+                                                  cancelButtonTitle: NSLocalizedString(@"OK", @"")
+                                                  otherButtonTitles: nil];
+            [alert show];
+            break;
+        }
+        default:
+            break;
+    }
+}
 
 @end
